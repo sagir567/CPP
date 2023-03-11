68bd6b162001fa32d610ba7df1a28405cb417d11
sagi's mac <sagir567@gmail.com>
2023-03-11 23:01:21 +0200

    sample of num types and their sizes

---
+++ intrudaction/nums_sizes.cpp
@@ -0,0 +1,24 @@
+
+//
+// Created by sagi yosef azulay on 11/03/2023.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+
+    cout << "int size in Bytes: " << sizeof(int) << endl;
+    cout << "unsigned int size in Bytes: " << sizeof(unsigned int) << endl;
+    cout << "short int size in Bytes: " << sizeof(short int) << endl;
+
+    cout << "\ndouble size in Bytes: " << sizeof(double) << endl;
+
+    cout << "\nfloat size in Bytes: " << sizeof(float) << endl;
+    cout << "\nlong size in Bytes: " << sizeof(long) << endl;
+
+
+
+}
